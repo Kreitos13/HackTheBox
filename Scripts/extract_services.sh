78e8e86a4da9781821942f92e20c44bc8160596c
Kr31tos <kr31tos@proton.me>
2024-02-06 14:07:14 +0100

    Se añade el script extract ports para extraer puertos y servicios

---
+++ Scripts/extract_services.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Extrae puertos y servicios que corre por ellos, hay que añadirle como parámetro el archivo del que se va a extraer
+
+
+# Verificar si se proporcionó un archivo como argumento
+if [ $# -eq 0 ]; then
+    echo "Uso: $0 archivo_nmap_output"
+    exit 1
+fi
+
+# Archivo de salida de Nmap
+archivo="$1"
+
+# Extraer puertos y servicios usando awk
+puertos=$(awk -F/ '/open/{print $1}' "$archivo" | column)
+servicios=$(grep "open" $archivo)
+
+# Imprimir los resultados
+echo "Puertos encontrados:"
+echo "$puertos"
+echo
+echo "Servicios encontrados:"
+echo "$servicios"
